01be10025866846f103694f0b867e4f6a1d92927
fomoFi.icu <joneclub1990@gmail.com>
2022-03-13 23:18:41 +0800

    嵌入图表

--- src/components/kLine/KLine.vue
+++ src/components/kLine/KLine.vue
@@ -1,4 +1,68 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { createChart } from 'lightweight-charts';
+import areaSeriesData from './mock/areaSeriesData';
+import volumeSeriesData from './mock/volumeSeriesData';
+import candlestickSeriesData from './mock/candlestickSeriesData';
+
+const chartRef = ref(null);
+onMounted(() => {
+    // DOM元素将在初始渲染后分配给ref
+    console.log('chartRef.value', chartRef.value); // <div>这是根元素</div>
+
+    try {
+        const chart = createChart(chartRef.value, {
+            width: 900,
+            height: 500,
+            rightPriceScale: {
+                scaleMargins: {
+                    top: 0.3,
+                    bottom: 0.25
+                },
+                borderVisible: false
+            },
+            layout: {
+                backgroundColor: '#242933',
+                textColor: '#d1d4dc'
+            },
+            grid: {
+                vertLines: {
+                    color: 'rgba(42, 46, 57, 0)'
+                },
+                horzLines: {
+                    color: 'rgba(42, 46, 57, 0.6)'
+                }
+            }
+        });
+        const areaSeries = chart.addAreaSeries({
+            topColor: 'rgba(38,198,218, 0.56)',
+            bottomColor: 'rgba(38,198,218, 0.04)',
+            lineColor: 'rgba(38,198,218, 1)',
+            lineWidth: 2
+        });
+
+        const volumeSeries = chart.addHistogramSeries({
+            color: '#26a69a',
+            priceFormat: {
+                type: 'volume'
+            },
+            priceScaleId: '',
+            scaleMargins: {
+                top: 0.8,
+                bottom: 0
+            }
+        });
+
+        areaSeries.setData(areaSeriesData);
+        volumeSeries.setData(volumeSeriesData);
+
+        // const candlestickSeries = chart.addCandlestickSeries();
+        // candlestickSeries.setData(candlestickSeriesData);
+    } catch (e) {
+        console.log('e', e);
+    }
+});
+</script>
 <template>
-    <div>kline</div>
+    <div ref="chartRef"></div>
 </template>
--- src/components/swap/Swap.vue
+++ src/components/swap/Swap.vue
@@ -1,4 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import KLine from '@/components/kLine/KLine.vue';
+</script>
 <template>
-    <div>swap</div>
+    <div>
+        <div class="flex flex-row m-4">
+            <div class="basis-3/4"><KLine /></div>
+            <div class="basis-1/4">
+                <div class="card w-full h-3/4 bg-base-200">
+                    <div class="card-body">
+                        <!-- <h2 class="card-title">Card title!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Buy Now</button>
+                    </div> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
