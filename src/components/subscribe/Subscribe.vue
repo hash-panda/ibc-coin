317ba4696a2d1103b669486e4a3a98958186bba8
fomoFi.icu <joneclub1990@gmail.com>
2022-03-13 21:36:34 +0800

    subscribe 响应式

--- src/components/subscribe/Subscribe.vue
+++ src/components/subscribe/Subscribe.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts"></script>
 <template>
     <div>
-        <div class="w-full flex justify-center mt-16">
-            <div class="w-1/2">
+        <div class="w-full flex justify-center mt-8 md:mt-16">
+            <div class="w-3/4 md:w-1/2">
                 <article class="prose dark:prose-invert text-center">
                     <h1>Stake-to-Subscribe</h1>
                     <p
@@ -19,11 +19,11 @@
                     value="40"
                     class="range range-primary mt-10"
                 />
-                <div class="mt-10">
+                <div class="mt-4">
                     <div class="form-control">
                         <label class="input-group">
                             <input
-                                type="number"
+                                type="text"
                                 placeholder="40"
                                 min="1"
                                 max="1000000"
@@ -35,7 +35,7 @@
                 </div>
             </div>
         </div>
-        <div class="grid gap-4 grid-cols-3 m-10">
+        <div class="grid gap-4 grid-cols-1 md:grid-cols-3 mx-10 lg:mx-32 my-10">
             <div class="basis-1/3 bg-base-200 h-96">320×568</div>
             <div class="basis-1/3 bg-base-200 h-96">320×568</div>
             <div class="basis-1/3 bg-base-200 h-96">320×568</div>
