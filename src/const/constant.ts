98aeb38052436dff4afdae0a435b0a8605d9a986
QingYiXiaoYao <wangtao_2016@126.com>
2022-03-16 04:06:25 +0800

    update Subscribe页面修改

---
+++ src/const/constant.ts
@@ -0,0 +1,89 @@
+const priceConfig = [
+  {
+    id: '9527',
+    title: 'INTERN',
+    minprice: '25',
+    coin: 'ATOM',
+    features: {
+      title: 'Key features',
+      items: [
+        'Realtime prices and charts',
+        'Pair analytics (volume/liquidity/APR)',
+        '15+ starred pairs',
+        '3+ active price alerts',
+        '155+ historical transactions',
+        '2.5%+ Hallswap V1 fee discount'
+      ]
+    },
+    steps: {
+      title: 'Coming soon',
+      items: [
+        'Portfolio tracker',
+        'Community voting',
+        'Whale trade alerts'
+      ]
+    }
+  },
+  {
+    id: '9528',
+    title: 'ENGINEER',
+    minprice: '100',
+    coin: 'ATOM',
+    features: {
+      title: 'Key features',
+      items: [
+        'Realtime prices and charts',
+        'Pair analytics (volume/liquidity/APR)',
+        '30+ starred pairs',
+        '15+ active price alerts',
+        '530+ historical transactions',
+        '10%+ Hallswap V1 fee discount'
+      ]
+    },
+    steps: {
+      title: 'Coming soon',
+      items: [
+        'Portfolio tracker',
+        'Community voting',
+        'Whale trade alerts',
+        'New and verified pairs alerts'
+      ]
+    }
+  },
+  {
+    id: '9529',
+    title: 'ASTRONAUT',
+    minprice: '1000',
+    coin: 'ATOM',
+    featuresTitle: 'Key features',
+    features: {
+      title: 'Key features',
+      items: [
+        'Realtime prices and charts',
+        'Pair analytics (volume/liquidity/APR)',
+        '210+ starred pairs',
+        '510+ active price alerts',
+        '5030+ historical transactions',
+        'No Hallswap V1 fee'
+      ]
+    },
+    steps: {
+      title: 'Coming soon',
+      items: [
+        'Portfolio tracker',
+        'Community voting',
+        'Whale trade alerts',
+        'Early access to new features',
+        'Exclusive Telegram group'
+      ]
+    }
+  },
+  {
+    id: '9530',
+    title: 'MOONWALKER',
+    minprice: '10,000',
+    coin: 'ATOM'
+  }
+]
+
+export default { priceConfig }
